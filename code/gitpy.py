8b12bac9ee933ca0ee94b83f68c0b0c0691697c5
babygame0ver <Akshatkathpal29@gmail.com>
2019-02-17 21:08:05 +0530

    upgraded the code for python3 and added a virtualenv for  gitpy. activate the virtualenv before working on the project

--- code/gitpy.py
+++ code/gitpy.py
@@ -9,7 +9,7 @@ import requests
 def get_access_token_from_directory():
     path = r'/home/baby/Desktop/github/account/access_token.txt'
     if path == None:
-        print "Please enter the path in the program"
+        print ("Please enter the path in the program")
         exit()
     access_token = open(path,'r').read().rstrip() # use rstrip() to remove trailing \n
     return access_token
@@ -33,25 +33,25 @@ class Github():
         required_link = Github.base_link_api + '/users/' + self.username # overiding required_link
         response = requests.get(required_link,headers=authorization_data)
 
-        # print response.headers # for debugging purpose
+        # print (response.headers # for debugging purpose)
         # for key in response.headers:
-        #     print key ,response.headers[key]
+        #     print (key ,response.headers[key])
 
         if response.headers['X-RateLimit-Limit'] == "5000" and response.headers['Status'] == "200 OK": # authorization will increase limit
             self.authorized = True
             self.authorization_data = {'Authorization':'token {}'.format(self.token)}
-            print 'Your Token is working Correctly and Username is also correct.'
+            print ('Your Token is working Correctly and Username is also correct.')
         else:
             if response.headers['Status'] == '401 Unauthorized':
-                print 'Your authentication token in not correct'
+                print ('Your authentication token in not correct')
             elif response.headers['Status'] == '404 Not Found':
-                print 'Enter your Username Correctly'
+                print ('Enter your Username Correctly')
 
     def get_user_repos(self): # get all repos in the users account only
         if self.authorized:
             pass
         else:
-            print self.authorization_error
+            print (self.authorization_error)
 
     def get_organization_repos(self,orgranization):
         ''' https://developer.github.com/v3/repos/#list-organization-repositories '''
@@ -60,10 +60,10 @@ class Github():
             org_link = '/orgs/{}/repos'.format(orgranization)
             required_link = Github.base_link_api + org_link # overiding required_link
             response = requests.get(required_link,headers=self.authorization_data)
-            # print response.status_code
-            print (response.json())[0]
+            # print (response.status_code)
+            print ((response.json())[0])
         else:
-            print self.authorization_error # get all the repositories in an organization
+            print (self.authorization_error) # get all the repositories in an organization)
 
     # main function for gitcrypt
     def create_a_repo_in_an_organization(self,organization_name,repo_name):
@@ -99,19 +99,19 @@ class Github():
 
             if response.status_code == 201: # success
                 new_repo_link = 'https://www.github.com/{}/{}'.format(organization_name,repo_name)
-                print 'repository has been created with name {} in orgranization {} at link {}'.format(repo_name,organization_name,new_repo_link)
+                print ('repository has been created with name {} in orgranization {} at link {}'.format(repo_name,organization_name,new_repo_link))
             elif response.status_code == 422: #failed
-                print 'repository creatio failed as the name is already existing'
+                print ('repository creatio failed as the name is already existing')
 
         else:
-            print self.authorization_error # create a repository in an organization
+            print (self.authorization_error) # create a repository in an organization)
 
     def create_a_file_in_repo(self,file_name):
         ''' https://developer.github.com/v3/repos/contents/#create-a-file '''
         if self.authorized:
             pass
         else:
-            print self.authorization_error # create a file in repository
+            print (self.authorization_error) # create a file in repository
 
 
 
