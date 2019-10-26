5331f87c68845d156f6009e4cdd5e9368943a4cf
Akshay Kathpal <Akshatkathpal29@gmail.com>
2019-10-26 19:35:54 +0530

    created function for loading initial configuration file

--- gitpy.py
+++ gitpy.py
@@ -1,23 +1,41 @@
-#!/usr/bin/env python3
-
-
-
+'''
+GitPy is a command line tool to consume the developer_api of github using authentication Token
+It shows all the public/private information for a github account with token.
+Functionality of the script
+1. Initial configuration
+2. Test authentication token for account using header
+'''
+import json
+import os
 import requests
 
-class GitHubPy:
+class GitPy:
 
-    access_token_path = 'path'
+    git_config_path = r'C:\Users\baby\Google Drive\meta-data\github\babygame0ver'
+    os.environ['gitpy_path'] = git_config_path
 
-    def __init__(self,username):
+    def __init__(self,username=None,password=None,token=None):
+        self.authorized = False
         self.username = username
+        self.password = password
+        self.token = None
 
     @staticmethod
-    def get_access_token():
-        ''' Get access token from local directory '''
-        return(GitHubPy.access_token_path)
+    def get_initial_configuraion():
+        '''Get Initial configuration from file'''
+        if GitPy.git_config_path is None:
+            print('Please specify the token path in Script')
+            return None
+        else:
+            config_path = os.environ['gitpy_path'] + '\config.json'
+            with open(config_path,'rb') as f:
+                dict = json.load(f)
+                return dict
 
 def main():
-    print(GitHubPy.get_access_token())
+    configuration = GitPy.get_initial_configuraion()
+    print(configuration.keys())
+    print('Username : ',configuration['username'])
 
 
 if __name__ == '__main__':
--- test_gitpy.py
+++ test_gitpy.py
@@ -1,6 +1,40 @@
+import json
 import logging
 import unittest
+import os
+from gitpy import GitPy
 
+class TestGitPy(unittest.TestCase):
 
-class TestGitHubPy(unittest.TestCase):
-    pass
+    logger = None
+
+    @classmethod
+    def setUpClass(cls):
+        ''' Setting up logger before testing whole script '''
+        file_name = os.path.basename(__file__).split('.')[0]
+        cls.logger = logging.Logger(file_name)
+        cls.logger.setLevel(logging.DEBUG)
+        file_handle = logging.FileHandler(file_name + '.log')
+        log_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handle.setFormatter(log_format)
+        cls.logger.addHandler(file_handle)
+        cls.logger.debug('Setting up Logger')
+
+        ''' Setting up config file path env'''
+        git_config_path = r'C:\Users\baby\Google Drive\meta-data\github\babygame0ver'
+        os.environ['gitpy_path'] = git_config_path
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.logger.debug('Shutting down logger')
+
+    def test_intial_configuration(self):
+        self.logger.info('calling test_intial_configuration')
+        config_file = os.environ['gitpy_path'] + '\config.json'
+        with open(config_file,'r') as f:
+            data = json.loads(f.read())
+        self.assertEqual(data,GitPy.get_initial_configuraion())
+        self.logger.info('completed test_intial_configuration')
+
+if __name__ == '__main__':
+    unittest.main()
