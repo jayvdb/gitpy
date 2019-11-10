f7d8c2cfbbc18a8073fd51e43dcd019f5340e7cf
Akshay Kathpal <Akshatkathpal29@gmail.com>
2019-11-10 18:15:51 +0530

    completed list list all branch functionality for a repository

--- README.md
+++ README.md
@@ -11,7 +11,9 @@ Python Interface to GitHub developer's API
 
 ## Dependencies
 
-[![Python](https://img.shields.io/badge/python-3.7.4-blue.svg)](https://www.python.org/downloads/release/python-374/) [![Requests](https://img.shields.io/badge/requests-2.22.0-blue.svg?style=flat-square)](https://pypi.python.org/pypi/requests/) [![Coverage](https://img.shields.io/badge/Coverage-4.5.4-blue.svg?style=flat-square)](https://pypi.org/project/coverage/)
+[![Python](https://img.shields.io/badge/python-3.7.4-blue.svg)](https://www.python.org/downloads/release/python-374/)
+[![Requests](https://img.shields.io/badge/requests-2.22.0-blue.svg?style=flat-square)](https://pypi.python.org/pypi/requests/)
+[![Coverage](https://img.shields.io/badge/Coverage-4.5.4-blue.svg?style=flat-square)](https://pypi.org/project/coverage/)
 
 
 ---
@@ -23,13 +25,25 @@ Python Interface to GitHub developer's API
 # git clone https://github.com/babygame0ver/gitpy.git && cd gitpy
 # pip install -r requirements.txt
 # python3 setup.py install
+
+```
+
+## Run test suite
+
+```
 # python3 -m unittest discover
+# coverage run -m unittest discover
 
 ```
 
 ---
-A command line package purely written in Python3 consumes GitHub developer's API and provides all the functionalities in one place.
+
+A command line package purely written in Python3 consumes GitHub developer's API and provides all the functionalities in one place using Python Function.
 
 * Core : Deals with authentication with GitHub API using Authentication token.
 
 * Repository : Deals with information & actions related to both public & private Repositories.
+
+---
+
+## Documentation
---
+++ tests/test_branches.py
@@ -0,0 +1,53 @@
+import json
+import logging
+import os
+import requests
+import unittest
+
+from gitpy.repository.branch.branches import branches
+from .initial_setup import initial_config_setup
+
+class TestBranches(unittest.TestCase):
+
+    logger = None
+    configuration_data = dict()
+    gitpy_object = None
+
+    @classmethod
+    def setUpClass(cls):
+        ''' Setting up logger before testing whole script '''
+        file_name = os.path.basename(__file__).split('.')[0]
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        log_file_path = os.path.join(dir_path,'logs\\') + file_name + '.log'
+        cls.logger = logging.Logger(file_name)
+        cls.logger.setLevel(logging.DEBUG)
+        file_handle = logging.FileHandler(log_file_path)
+        log_format = logging.Formatter('%(asctime)s - %(levelname)s - %(funcName)s: - %(message)s')
+        file_handle.setFormatter(log_format)
+        cls.logger.addHandler(file_handle)
+        cls.logger.debug('Setting up Logger')
+        cls.configuration_data = initial_config_setup()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.logger.debug('Shutting down logger\n')
+
+    def setUp(self):
+        self.logger.info('executing')
+        self.branch_object = branches()
+        self.logger.info('completed')
+
+    def test_get_all_branches_of_a_repo(self):
+        self.logger.info('executing')
+        msg = ''
+        repo_name = 'repository-two'
+        required_link = self.branch_object.gitpy_object.developer_api + '/repos/{}/{}/branches'.format(self.branch_object.username,repo_name)
+        try:
+            session = requests.session()
+            response = session.get(required_link,headers=self.branch_object.gitpy_object.authorization_data)
+            msg = response.json()
+            session.close()
+        except requests.exceptions.RequestException as e:
+            msg = 'Please connect to Internet'
+        self.assertEqual(self.branch_object.get_all_branches_of_a_repo(repo_name),msg)
+        self.logger.info('completed')
