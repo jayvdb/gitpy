24b5869c178c086c2dce642ac963eac233e34861
Akshay Kathpal <Akshatkathpal29@gmail.com>
2019-12-14 14:01:12 +0530

    added codebase to post install script

--- tests/after_install/post_install_script.py
+++ tests/after_install/post_install_script.py
@@ -1,8 +1,15 @@
 from gitpy.core.auth import GitPy
 
-def main():
+def authentication_module():
     config_data = GitPy.get_initial_configuration()
     print(config_data)
+    g = GitPy(username = config_data['username'], token = config_data['token'])
+    '''
+    or
+    g = GitPy(username = 'username',token = 'token')
+    '''
+    print(g.check_connectivity()) # Connected
+    print(g.authorization()) # Authorization Successfull {username}
 
 if __name__ == '__main__':
     main()
